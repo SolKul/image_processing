4e6b2ff8a7c309232e281e9105b0d0df75ad1639
SolKul <46105598+SolKul@users.noreply.github.com>
2022-10-23 16:43:54 +0000

    [feat] point out impl of Camera.factor

--- impro/camera.py
+++ impro/camera.py
@@ -24,7 +24,7 @@ class Camera:
     Args:
         P (array):カメラ行列P=K[R|t]。
     '''
-    def __init__(self,P):
+    def __init__(self,P:np.ndarray):
         self.P=P
         self.K=None 
         self.R=None
@@ -34,13 +34,45 @@ class Camera:
     def project(self,X):
         x=self.P @ X
         x=x/x[2]
-        return x    
-    def factor(self):
-        K,R=linalg.rq(self.P[:,:3])
-        T=np.diag(np.sign(np.diag(K)))
+        return x   
+
+    def factor(self)->tuple[np.ndarray,np.ndarray,np.ndarray]:
+        """P=K[R|t]。に従い、カメラ行列をK,R,tに分解する
+
+        Returns:
+            tuple[np.ndarray,np.ndarray,np.ndarray]: _description_
+        """
+        # 最初の3x3の部分を分解する
+        mtx_k,mtx_r=linalg.rq(self.P[:,:3])
+        # Kの対角成分が正になるようにする
+        mtx_t=np.diag(np.sign(np.diag(mtx_k)))
+        # 以下2行の条件分岐は不要では？
+        # if np.linalg.det(mtx_t) < 0:
+        #     mtx_t[1,1] *= -1
         #K,Rの行列式を正にする
-        self.K= np.dot(K,T)
-        self.R= np.dot(T,R)
+        self.K= np.dot(mtx_k,mtx_t)
+        self.R= np.dot(mtx_t,mtx_r)
+        self.t= np.linalg.inv(self.K) @ self.P[:,3]
+        
+        return self.K, self.R, self.t
+
+    def factor_solem(self)->tuple[np.ndarray,np.ndarray,np.ndarray]:
+        """P=K[R|t]。に従い、カメラ行列をK,R,tに分解する、教科書通りのじ実装
+        不要な条件分岐がある
+
+        Returns:
+            tuple[np.ndarray,np.ndarray,np.ndarray]: _description_
+        """
+        # 最初の3x3の部分を分解する
+        mtx_k,mtx_r=linalg.rq(self.P[:,:3])
+        # Kの対角成分が正になるようにする
+        mtx_t=np.diag(np.sign(np.diag(mtx_k)))
+        # 以下2行の条件分岐は不要では？
+        if np.linalg.det(mtx_t) < 0:
+            mtx_t[1,1] *= -1
+        #K,Rの行列式を正にする
+        self.K= np.dot(mtx_k,mtx_t)
+        self.R= np.dot(mtx_t,mtx_r)
         self.t= np.linalg.inv(self.K) @ self.P[:,3]
         
         return self.K, self.R, self.t
@@ -53,11 +85,17 @@ class Camera:
             self.factor()
             self.c= - self.R.T @ self.t
             return self.c
+            
     
 def rotation_matrix(a):
     """ ベクトルaを軸に回転する3Dの回転行列を返す """
     R = np.eye(4)
-    R[:3,:3] = linalg.expm([[0,-a[2],a[1]],[a[2],0,-a[0]],[-a[1],a[0],0]])
+    R[:3,:3] = linalg.expm(
+        [
+            [0    ,-a[2], a[1]],
+            [a[2] ,0    ,-a[0]],
+            [-a[1],a[0] ,0    ]
+        ])
     return R
 
 def calculate_camera_matrix_w_sz(sz,sz_orig=(6000,4000),lens='PZ',f_orig=(4188,4186)):
